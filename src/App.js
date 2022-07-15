5b22dfa9ad7f6ef5f3f57d3e0af8f5f0d3110975
dorcas-1995 <wayumulu58@gmail.com>
2022-07-15 16:15:22 -0400

    assignment commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
 
-function App() {
+import React from "react";
+
+function NavBar() {
   return (
-    <div>
-      <Article />
-      <Comment />
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About /> 
+    </div>
+  );
+}
+
+export default App;;
--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,9 @@
-import React from "react";
-
-function Article() {
-  return (
+ function Article() {
+   return (
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
+function Comment() {
+   return <div>Naturally, I agree with this article.</div>;
 
-export default Article;
+  }
